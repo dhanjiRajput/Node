86a5123cafb5c1d8698562c83f0e9fab7f43feab
dhanjiRajput <kidechadhanji@gmail.com>
2024-12-17 21:21:47 +0530

    bcrypt

--- Practical-5_Auhthorizaton/Controller/routes_method.js
+++ Practical-5_Auhthorizaton/Controller/routes_method.js
@@ -1,4 +1,5 @@
-  const User = require("../Model/user")
+  const User = require("../Model/user");
+  const bcrypt = require("bcrypt");
 
 const login=(req,res)=>{
     res.render('login');
@@ -10,11 +11,13 @@ const signup=(req,res)=>{
 
 const createUser=async(req,res)=>{
     try {
-        const { email } = req.body;
+        const { email,password } = req.body;
         let isExists = await User.findOne({ email: email });
         if (isExists) {
           return res.send("users already Exists");
         } else {
+          let hashedPassword = await bcrypt.hash(password, 10);
+          req.body.password = hashedPassword;
           let user = await User.create(req.body);
           return res.status(201).json(user);
         }
@@ -54,7 +57,8 @@ const loginUser=async(req,res)=>{
     return res.send("User not found...");
   }
 
-  if(isExist.password!=password){
+  const ismatched=await bcrypt.compare(password,isExist.password);
+  if(!ismatched){
     return res.send("Invalid password...");
   }
   res.cookie('username', isExist.username);
